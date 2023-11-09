64b27ec7a9a3e4343518cc74e955d4d81d6eceee
Dane Hoffman <dhoffman@Danes-MacBook-Air.local>
2023-11-09 08:37:28 -0700

    window.addEventListener('load') to create an HTML upon page loading; use to create a 'card'.

--- code.js
+++ code.js
@@ -1,2 +1,17 @@
+
+const body = document.querySelector('body');
+const  main = document.querySelector('main');
+console.log(body);
+console.log(main);
+
+//Create a 'card' and render it to HTML
+//
+window.addEventListener('load', () => {
+    const mainMessage = document.createElement('div');
+    mainMessage.textContent = `Hello World!`;
+    main.append(mainMessage);
+
+});
+
 const sayHello = "Hello!";
 console.log(sayHello);
