ae867cfed8ebe0a19f0104c463fbc03afb8ab83a
Dane Hoffman <dhoffman@Danes-MacBook-Air.local>
2023-11-09 09:42:38 -0700

    Grabbed <button> console.log 'Button Clicked' when clicked.

--- code.js
+++ code.js
@@ -2,11 +2,15 @@
 const body = document.querySelector('body');
 const  main = document.querySelector('main');
 //Grab the button
+const button = document.querySelector('button');
+console.log (button);
 
 const puppyName = "${puppyName}"
 
 //Create a 'card' and render it to HTML
 window.addEventListener('load', () => {
+
+    //Create the 'mainCard' element
     const mainCard = document.createElement('section');
     mainCard.innerHTML = `
     <h4>Puppy: ${puppyName}</h4>
@@ -23,6 +27,13 @@ window.addEventListener('load', () => {
     mainCard.style.display = 'flex';
     mainCard.style.flexDirection = 'column';
     mainCard.style.width = '25%';
+});
+
+//Make the HMTL button dynamic
+button.addEventListener('click', () => {
+    const buttonHasBeenClicked  = 'I have been clicked!';
+    console.log(buttonHasBeenClicked);
+
 });
 
 console.log(main);
