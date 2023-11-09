471a8e1a46a40ad173310d66b1e5f877f6402096
Dane Hoffman <dhoffman@Danes-MacBook-Air.local>
2023-11-09 10:56:52 -0700

    Completed API call to retrieve .data.players; provides an array of objects.

--- code.js
+++ code.js
@@ -2,22 +2,24 @@
 const body = document.querySelector('body');
 const  main = document.querySelector('main');
 const button = document.querySelector('button');
-
-const puppyName = "${puppyName}";
-
-//Create a 'card' and render it to HTML
-window.addEventListener('load', () => {
-
-    
-
-
-    //Use JavaScript to manipulate CSS of <button>
-    button.textContent = "Click for MORE Puppies!";
-});
+button.textContent = "Click to view a Puppy Card";
 
 //Make the HMTL button dynamic
 button.addEventListener('click', async() => {
+    //Change text of main <button> to say 'Go Back'once clicked
+    button.textContent = 'Go Back';
+
+    //API Call
+    const puppyBowlAPICall  = await fetch ('https://fsa-puppy-bowl.herokuapp.com/api/2310-FSA-ET-WEB-FT-SF/players');
+    const puppyBowlAPICallResult = await puppyBowlAPICall.json();
+    const puppyBowlAPIData = puppyBowlAPICallResult.data;
+    const puppyPlayers = puppyBowlAPIData.players;
+
+    console.log(puppyPlayers);
+    const puppyName = `Scooby Doo`;
+
     //Create the 'mainCard' element
+    //Set the puppyName equal to the name of a puppy object from the API
     const mainCard = document.createElement('section');
     mainCard.innerHTML = `
     <h4>Puppy: ${puppyName}</h4>
@@ -26,18 +28,17 @@ button.addEventListener('click', async() => {
     Proin sollicitudin egestas erat, eu.</p>
                             `;
     main.append(mainCard);
-
+    
     //Use JavaScript to manipulate CSS of <section>
     mainCard.style.backgroundColor = 'lightblue';
     mainCard.style.display = 'flex';
     mainCard.style.flexDirection = 'column';
     mainCard.style.width = '25%';
 
-    const puppyBowlAPICall  = await fetch ('https://fsa-puppy-bowl.herokuapp.com/api/2310-FSA-ET-WEB-FT-SF/players');
-    const puppyBowlAPICallResult = await puppyBowlAPICall.json();
+    //On click, Create another button to 'view MORE puppies'
+        //Displays a list of all puppies in the roster (make the mainCard dissappear)
+            //Clicking on single puppy in list sets the mainCard values to that puppy
 
-    console.log(puppyBowlAPICallResult);
-    button.textContent = 'Go Back';
 
 });
 
