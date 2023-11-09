431f70866f1a71b43fc928000b4239d7f126045b
Dane Hoffman <dhoffman@Danes-MacBook-Air.local>
2023-11-09 09:32:50 -0700

    Used js to .style the main in order to create a 'card'.

--- code.js
+++ code.js
@@ -1,10 +1,11 @@
 
 const body = document.querySelector('body');
 const  main = document.querySelector('main');
+//Grab the button
+
 const puppyName = "${puppyName}"
 
 //Create a 'card' and render it to HTML
-//
 window.addEventListener('load', () => {
     const mainCard = document.createElement('section');
     mainCard.innerHTML = `
@@ -16,8 +17,12 @@ window.addEventListener('load', () => {
     main.append(mainCard);
 
     //Use JavaScript to manipulate CSS of an element
-    main.style.backgroundColor = 'blue';
-    main.style.display = 'inline-block';
+    main.style.backgroundColor = 'lightblue';
+    // main.style.display = 'inline-block';
+    // main.style.width = '15 px';
+    main.style.display = 'flex';
+    main.style.flexDirection = 'column';
+    main.style.width = '25%';
 });
 
 console.log(main);
