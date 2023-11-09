9a10e8b20fbd0d4804ad9b0a379265ddb7bc4082
Dane Hoffman <dhoffman@Danes-MacBook-Air.local>
2023-11-09 12:40:35 -0700

    Page load, view a puppy card fetches a random puppy also creating two new buttons, view a random puppy will fetch a new random puppy, original button turns to goback and will return to original screen.

--- code.js
+++ code.js
@@ -1,70 +1,83 @@
-
 const body = document.querySelector('body');
-const  main = document.querySelector('main');
+const main = document.querySelector('main');
 const button = document.querySelector('button');
 button.textContent = "Click to view a Puppy Card";
 
-//Make the HMTL button dynamic
-button.addEventListener('click', async() => {
-    //Change text of main <button> to say 'Go Back'once clicked
+// Function to create a new puppy card
+const createPuppyCard = (puppy) => {
     button.textContent = 'Go Back';
 
-    //API Call
-    const puppyBowlAPICall  = await fetch ('https://fsa-puppy-bowl.herokuapp.com/api/2310-FSA-ET-WEB-FT-SF/players');
-    const puppyBowlAPICallResult = await puppyBowlAPICall.json();
-    const puppyBowlAPIData = puppyBowlAPICallResult.data;
-    const puppyPlayers = puppyBowlAPIData.players;
-    console.log(puppyPlayers);
-
-    //Select a random puppyPlayer from the puppyPlayers[], provide that name in the 'card.'
-    const singlePuppyPlayer = Math.floor(Math.random() * puppyPlayers.length);
-    const puppyName = puppyPlayers[singlePuppyPlayer].name;
-    const puppyBreed = puppyPlayers[singlePuppyPlayer].breed;
-    const puppyPicture = puppyPlayers[singlePuppyPlayer].imageUrl;
-
-    console.log(puppyName);
-
-    //Create the 'mainCard' element
-    //Set the puppyName equal to the name of a puppy object from the API
+    // Create the 'mainCard' element
     const mainCard = document.createElement('section');
-    mainCard.innerHTML = '';
     mainCard.innerHTML = `
-    <h4>Puppy: ${puppyName}</h4>
-    <h4>Breed: ${puppyBreed}</h4>
-    <img src=${puppyPicture} alt="Picture of a puppy"/>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-    Donec ut ante nec justo eleifend venenatis.
-    Proin sollicitudin egestas erat, eu.</p>
-    <button id=randomPuppy> ViewARandomPuppy </button>
-    <button id=allPuppies> View ALL Puppies </button>
-                            `;
+        <h4>Puppy: ${puppy.name}</h4>
+        <h4>Breed: ${puppy.breed}</h4>
+        <img src=${puppy.imageUrl} alt="Picture of a puppy"/>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        Donec ut ante nec justo eleifend venenatis.
+        Proin sollicitudin egestas erat, eu.</p>
+        <button id=randomPuppy> View A Random Puppy </button>
+        <button id=allPuppies> View ALL Puppies </button>
+    `;
     main.append(mainCard);
-    
-    //Use JavaScript to manipulate CSS of <section>
     mainCard.style.backgroundColor = 'lightblue';
     mainCard.style.display = 'flex';
     mainCard.style.flexDirection = 'column';
     mainCard.style.width = '25%';
 
-    //Click View Random Puppy, selects another random puppy
+    // Click View Random Puppy, selects another random puppy
     const randomPuppyButton = document.querySelector(`#randomPuppy`);
-    randomPuppyButton.addEventListener('click', () => {
-        mainCard.innerHTML = ``;
-        console.log('randomPuppy');
+    randomPuppyButton.addEventListener('click', async () => {
+        // Clear the current content of mainCard for a new Puppy
 
+        main.innerHTML = '';
+        
+        // API Call for a new set of puppies
+        const puppyBowlAPICall = await fetch('https://fsa-puppy-bowl.herokuapp.com/api/2310-FSA-ET-WEB-FT-SF/players');
+        const puppyBowlAPICallResult = await puppyBowlAPICall.json();
+        const puppyBowlAPIData = puppyBowlAPICallResult.data;
+        const puppyPlayers = puppyBowlAPIData.players;
+
+        // Select a random puppyPlayer from the puppyPlayers[], provide that name in the 'card.'
+        const newSinglePuppyPlayer = Math.floor(Math.random() * puppyPlayers.length);
+        const newPuppy = puppyPlayers[newSinglePuppyPlayer];
+        // Create the new 'mainCard' element
+        createPuppyCard(newPuppy);
     });
 
-    //Displays a list of all puppies in the roster (make the mainCard dissappear)
-    const allPupppiesButton = document.querySelector(`#allPuppies`);
-    allPupppiesButton.addEventListener('click', () => {
+    // Displays a list of all puppies in the roster (make the mainCard disappear)
+    const allPuppiesButton = document.querySelector(`#allPuppies`);
+    allPuppiesButton.addEventListener('click', () => {
         console.log(`allPuppies`);
     });
-        //Clicking on single puppy in list sets the mainCard values to that puppy
 
-    //Reset the mainCard element each time the 'newPuppy' <button> is clicked
-    // mainCard.innerHTML = '';
+    return mainCard;
+};
 
+// Save the original state of the page
+const originalState = main.innerHTML;
+
+// Make the HTML button dynamic
+button.addEventListener('click', async () => {
+    if (button.textContent === 'Go Back') {
+        // Reset the page to its original state
+        main.innerHTML = originalState;
+        button.textContent = "Click to view a Puppy Card";
+    } else {
+        // Change text of main <button> to say 'Go Back' once clicked
+        button.textContent = 'Go Back';
+
+        // API Call
+        const puppyBowlAPICall = await fetch('https://fsa-puppy-bowl.herokuapp.com/api/2310-FSA-ET-WEB-FT-SF/players');
+        const puppyBowlAPICallResult = await puppyBowlAPICall.json();
+        const puppyBowlAPIData = puppyBowlAPICallResult.data;
+        const puppyPlayers = puppyBowlAPIData.players;
+
+        // Select a random puppyPlayer from the puppyPlayers[], provide that name in the 'card.'
+        const singlePuppyPlayer = Math.floor(Math.random() * puppyPlayers.length);
+        const puppy = puppyPlayers[singlePuppyPlayer];
+
+        // Create the 'mainCard' element
+        createPuppyCard(puppy);
+    }
 });
-
-
-console.log(main);
